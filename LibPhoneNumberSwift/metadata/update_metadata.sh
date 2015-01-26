fcc82d0d3ab97db7c945e9f557acc80ae2ce3cb3
Hyuk Hur <hyukhur@gmail.com>
2015-01-26 10:56:42 +0900

    PhoneNumberMetadata.plist dose not in the repository and will be downloaded at first time and cached.

--- LibPhoneNumberSwift/metadata/update_metadata.sh
+++ LibPhoneNumberSwift/metadata/update_metadata.sh
@@ -1,4 +1,13 @@
 #! /bin/sh
+ORIGIN_URL=https://raw.githubusercontent.com/googlei18n/libphonenumber/master/resources/PhoneNumberMetadata.xml
+WORK_DIR=$SRCROOT/metadata
+METADATA_FILENAME=PhoneNumberMetadata.plist
+DATAFILE=$WORK_DIR/$METADATA_FILENAME
 
-xsltproc ../../libphonenumber-swift/libphonenumber2plist.xsl ./PhoneNumberMetadataForTesting.xml | plutil -
-curl -fsSL https://raw.githubusercontent.com/googlei18n/libphonenumber/master/resources/PhoneNumberMetadata.xml | xsltproc xml2plist.xsl - | plutil -convert json - -o PhoneNumberMetadata.plist
+if [ -e $DATAFILE ]
+then
+    echo "Meta Data Cached in" $DATAFILE
+else
+    echo "Download Meta Data to" $DATAFILE
+    curl -fsSL $ORIGIN_URL | xsltproc $WORK_DIR/xml2plist.xsl - | plutil -convert json - -o $DATAFILE
+fi
